77a3e8df7a2462cb4d3bd6fd791dfe1a6fba2271
Michal Margetín <86981961+SharkStudiosSK@users.noreply.github.com>
2024-02-28 13:17:22 +0100

    Update metadata.js

--- metadata.js
+++ metadata.js
@@ -2,5 +2,5 @@
 "name": "MeliskoCoin",
 "symbol": "MLC",
 "description": "For my friends",
-"image": "LINK_TO_TOKEN_IMAGE"
+"image": "https://raw.githubusercontent.com/SharkStudiosSK/SharkImages/main/meliskocoin_logo.png"
 }
